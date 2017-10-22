d00c3456f471efc7d43b619f7880c06a133a9b8f
Masaharu Komuro <komuro-m1@asahi.com>
2017-10-22 22:17:10 +0900

    途中保存

--- src/app/eventya/common/registry.service.ts
+++ src/app/eventya/common/registry.service.ts
@@ -5,7 +5,9 @@ export class RegistryService {
 
   constructor() { }
   
-  public title:     string = 'いべんとや';
-  public copyRight: string = 'Copyright © いべんとや 2017';
+  public title:        string = 'いべんとや';
+  public sp_title:     string = 'いべんとや | イベント用WEBアプリ専門店';
+  public subtitle:     string = 'イベント用webアプリ専門店';
+  public copyRight:    string = 'Copyright © いべんとや 2017';
 
 }
--- src/app/eventya/top/top.component.ts
+++ src/app/eventya/top/top.component.ts
@@ -10,12 +10,31 @@ export class TopComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  public filler_on_carousel: boolean = false;
 
   constructor (
       public registryService: RegistryService
   ) {
   
   }
+  
+  public slider_items: Array<any> = [
+    {
+      img_path: '../../../assets/img/top/slider/bingo.jpg',
+      title: '安定・王道の BINGO に一工夫！',
+      sp_title: '安定・王道の BINGO に一工夫！',
+      description: '来場者一人一人の写真を使った、思い出に残るビンゴゲームです',
+      url: 'games/bingo'
+    },
+    {
+      img_path: '../../../assets/img/top/slider/bingo.jpg',
+      title: '安定・王道の BINGO に一工夫！',
+      sp_title: '安定・王道の BINGO に一工夫！',
+      description: '来場者一人一人の写真を使った、思い出に残るビンゴゲームです',
+      url: 'games/bingo'
+    },
+  ];
   
 
 }
